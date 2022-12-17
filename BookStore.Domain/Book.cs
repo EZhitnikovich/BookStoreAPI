d2153af2897d077a2aed3dc631aa7d2878f33459
EZhitnikovich <ezhitnikovich@gmail.com>
2022-12-17 22:04:34 +0300

    edit Book.cs

--- BookStore.Domain/Book.cs
+++ BookStore.Domain/Book.cs
@@ -4,6 +4,6 @@ internal class Book : Entity
 {
     public Guid CategoryId { get; set; }
     public string Name { get; set; }
-    public string Desctiption { get; set; }
-
+    public string Description { get; set; }
+    public decimal Price { get; set; }
 }
