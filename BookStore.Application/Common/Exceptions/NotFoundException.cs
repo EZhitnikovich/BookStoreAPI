7e1611884adcb4b4a5030474b5c7323d8edea661
EZhitnikovich <ezhitnikovich@gmail.com>
2022-12-18 14:07:49 +0300

    Add custom exception

---
+++ BookStore.Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace BookStore.Application.Common.Exceptions
+{
+    internal class NotFoundException : Exception
+    {
+        public NotFoundException(string name, object key)
+            : base($"Entity \"{name}\" ({key}) not found.") { }
+    }
+}
