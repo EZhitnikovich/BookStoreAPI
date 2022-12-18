826929c9c23721fe084efba68130a261b97fc3c8
EZhitnikovich <ezhitnikovich@gmail.com>
2022-12-18 15:36:01 +0300

    Add UpdateRatingCommandHandler.cs

---
+++ BookStore.Application/Ratings/Commands/UpdateRating/UpdateRatingCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace BookStore.Application.Ratings.Commands.UpdateRating
+{
+    internal class UpdateRatingCommand : IRequest
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public int Value { get; set; }
+    }
+}
---
+++ BookStore.Application/Ratings/Commands/UpdateRating/UpdateRatingCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using BookStore.Application.Common.Exceptions;
+using BookStore.Application.Interfaces;
+using BookStore.Domain;
+using MediatR;
+
+namespace BookStore.Application.Ratings.Commands.UpdateRating
+{
+    internal class UpdateRatingCommandHandler
+        : IRequestHandler<UpdateRatingCommand>
+    {
+        private readonly IBookStoreDbContext context;
+
+        public UpdateRatingCommandHandler(IBookStoreDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Unit> Handle(UpdateRatingCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await context.Ratings.FindAsync(new object[] { request.Id }, cancellationToken);
+
+            if (entity == null || entity.UserId != request.UserId)
+            {
+                throw new NotFoundException(nameof(Rating), request.Id);
+            }
+
+            entity.Value = request.Value;
+            entity.EditDate = DateTime.Now;
+
+            await context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
