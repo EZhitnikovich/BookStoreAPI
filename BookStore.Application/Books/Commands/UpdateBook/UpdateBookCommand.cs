aef703645a79c4911a4edbe0452adec354d7bdf0
EZhitnikovich <ezhitnikovich@gmail.com>
2022-12-18 14:08:01 +0300

    Add UpdateBookCommandHandler.cs

---
+++ BookStore.Application/Books/Commands/UpdateBook/UpdateBookCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.Books.Commands.UpdateBook
+{
+    internal class UpdateBookCommand: IRequest
+    {
+        public Guid Id { get; set; }
+        public Guid CategoryId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ BookStore.Application/Books/Commands/UpdateBook/UpdateBookCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using BookStore.Application.Common.Exceptions;
+using BookStore.Application.Interfaces;
+using BookStore.Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Application.Books.Commands.UpdateBook
+{
+    internal class UpdateBookCommandHandler
+        : IRequestHandler<UpdateBookCommand>
+    {
+        private readonly IBookStoreDbContext context;
+
+        public UpdateBookCommandHandler(IBookStoreDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Unit> Handle(UpdateBookCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await context.Books.FirstOrDefaultAsync(x =>
+                x.Id == request.Id, cancellationToken);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(Book), request.Id);
+            }
+
+            entity.Title = request.Title;
+            entity.Description = request.Description;
+            entity.Price= request.Price;
+            entity.CategoryId = request.CategoryId;
+            entity.EditDate = DateTime.Now;
+
+            await context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
