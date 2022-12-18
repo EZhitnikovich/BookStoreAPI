924dada708fb5e570da05100da274ab13037e66e
EZhitnikovich <ezhitnikovich@gmail.com>
2022-12-18 15:09:10 +0300

    add UpdateCategoryCommandHandler.cs

---
+++ BookStore.Application/Categories/Commands/UpdateCategory/UpdateCategoryCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using BookStore.Application.Common.Exceptions;
+using BookStore.Application.Interfaces;
+using BookStore.Domain;
+using MediatR;
+
+namespace BookStore.Application.Categories.Commands.UpdateCategory
+{
+    internal class UpdateCategoryCommandHandler
+        : IRequestHandler<UpdateCategoryCommand>
+    {
+        private readonly IBookStoreDbContext context;
+
+        public UpdateCategoryCommandHandler(IBookStoreDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Unit> Handle(UpdateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await context.Categories.FindAsync(new object[] { request.Id }, cancellationToken);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(Category), request.Id);
+            }
+
+            entity.Title = request.Title;
+            entity.Description = request.Description;
+            entity.EditDate = DateTime.Now;
+
+            await context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
