bd01fb583372a7d168444ebac61866cfc0679d2e
EZhitnikovich <ezhitnikovich@gmail.com>
2022-12-18 15:11:52 +0300

    Add DeleteCategoryCommandHandler.cs

---
+++ BookStore.Application/Categories/Commands/DeleteCategory/DeleteCategoryCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace BookStore.Application.Categories.Commands.DeleteCategory
+{
+    internal class DeleteCategoryCommand : IRequest
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ BookStore.Application/Categories/Commands/DeleteCategory/DeleteCategoryCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using BookStore.Application.Common.Exceptions;
+using BookStore.Application.Interfaces;
+using BookStore.Domain;
+using MediatR;
+
+namespace BookStore.Application.Categories.Commands.DeleteCategory
+{
+    internal class DeleteCategoryCommandHandler
+        : IRequestHandler<DeleteCategoryCommand>
+    {
+        private readonly IBookStoreDbContext context;
+
+        public DeleteCategoryCommandHandler(IBookStoreDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await context.Categories.FindAsync(new object[] { request.Id }, cancellationToken);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(Category), request.Id);
+            }
+
+            context.Categories.Remove(entity);
+            await context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
