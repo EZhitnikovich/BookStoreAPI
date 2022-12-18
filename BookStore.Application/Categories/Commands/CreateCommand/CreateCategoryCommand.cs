781486959db0c31f7508af45e676eae5af3e2c26
EZhitnikovich <ezhitnikovich@gmail.com>
2022-12-18 15:01:06 +0300

    add CreateCategoryCommandHandler.cs

---
+++ BookStore.Application/Categories/Commands/CreateCommand/CreateCategoryCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace BookStore.Application.Categories.Commands.CreateCommand
+{
+    internal class CreateCategoryCommand: IRequest<Guid>
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ BookStore.Application/Categories/Commands/CreateCommand/CreateCategoryCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using BookStore.Application.Interfaces;
+using BookStore.Domain;
+using MediatR;
+
+namespace BookStore.Application.Categories.Commands.CreateCommand
+{
+    internal class CreateCategoryCommandHandler
+        : IRequestHandler<CreateCategoryCommand, Guid>
+    {
+        private readonly IBookStoreDbContext context;
+
+        public CreateCategoryCommandHandler(IBookStoreDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Guid> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var category = new Category
+            {
+                Title = request.Title,
+                Description = request.Description,
+                Id = Guid.NewGuid(),
+                CreationDate = DateTime.Now,
+                EditDate = null
+            };
+
+            await context.Categories.AddAsync(category, cancellationToken);
+            await context.SaveChangesAsync(cancellationToken);
+
+            return category.Id;
+        }
+    }
+}
