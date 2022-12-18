1b19997f74d320be5b25954f76fb7717c94db138
EZhitnikovich <ezhitnikovich@gmail.com>
2022-12-18 13:55:13 +0300

    add CreateBookCommandhandler

---
+++ BookStore.Application/Books/Commands/CreateBook/CreateBookCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace BookStore.Application.Books.Commands.CreateBook
+{
+    internal class CreateBookCommand : IRequest<Guid>
+    {
+        public Guid CategoryId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ BookStore.Application/Books/Commands/CreateBook/CreateBookCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using BookStore.Application.Interfaces;
+using BookStore.Domain;
+using MediatR;
+
+namespace BookStore.Application.Books.Commands.CreateBook
+{
+    internal class CreateBookCommandHandler
+        : IRequestHandler<CreateBookCommand, Guid>
+    {
+        private readonly IBookStoreDbContext context;
+        public CreateBookCommandHandler(IBookStoreDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Guid> Handle(CreateBookCommand request, CancellationToken cancellationToken)
+        {
+            var book = new Book
+            {
+                CategoryId = request.CategoryId,
+                Title = request.Title,
+                Description = request.Description,
+                Price = request.Price,
+                Id = Guid.NewGuid(),
+                CreationDate = DateTime.Now,
+                EditDate = null
+            };
+
+            await context.Books.AddAsync(book, cancellationToken);
+            await context.SaveChangesAsync(cancellationToken);
+
+            return book.Id;
+        }
+    }
+}
