e2573255d8bc8ac6039133958311fff5cffd92f7
EZhitnikovich <ezhitnikovich@gmail.com>
2022-12-18 14:16:22 +0300

    add DeleteBookCommandHandler.cs

---
+++ BookStore.Application/Books/Commands/DeleteBook/DeleteBookCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace BookStore.Application.Books.Commands.DeleteBook
+{
+    internal class DeleteBookCommand : IRequest
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ BookStore.Application/Books/Commands/DeleteBook/DeleteBookCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using BookStore.Application.Common.Exceptions;
+using BookStore.Application.Interfaces;
+using BookStore.Domain;
+using MediatR;
+
+namespace BookStore.Application.Books.Commands.DeleteBook
+{
+    internal class DeleteBookCommandHandler
+        : IRequestHandler<DeleteBookCommand>
+    {
+        public IBookStoreDbContext context { get; set; }
+
+        public DeleteBookCommandHandler(IBookStoreDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteBookCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await context.Books.FindAsync(new object[] { request.Id }, cancellationToken);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(Book), request.Id);
+            }
+
+            context.Books.Remove(entity);
+            await context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
