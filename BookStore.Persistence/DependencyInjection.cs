6743f6a0a478b8583d360cf26680ea752d6c1ae0
EZhitnikovich <ezhitnikovich@gmail.com>
2022-12-19 22:09:04 +0300

    add DependencyInjection.cs

---
+++ BookStore.Persistence/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using BookStore.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BookStore.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services,
+                                                    IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<BookStoreDbContext>(opt =>
+            {
+                opt.UseSqlite(connectionString);
+            });
+
+            services.AddScoped<IBookStoreDbContext, BookStoreDbContext>(); //TODO
+            return services;
+        }
+    }
+}
